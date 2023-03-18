5acca884b387cee0f55e46e9442f4feb9c04c3f0
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-03-18 20:18:20 +0100

    18.03.23

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -6,7 +6,12 @@ function getAbout(req, res) {
   res.render('./about');
 }
 
+function getMenu(req, res) {
+  res.render('./menu');
+}
+
 module.exports = {
   getIndex: getIndex,
   getAbout: getAbout,
+  getMenu: getMenu,
 };
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', baseController.getIndex);
 router.get('/about', baseController.getAbout)
+router.get('/menu', baseController.getMenu)
 
 module.exports = router;
