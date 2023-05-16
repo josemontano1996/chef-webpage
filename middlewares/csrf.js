a90d7560d499fab66b6d0dbc63dff0c877bef326
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-05-16 14:56:41 +0200

    fixed bug with csrf middleware

--- middlewares/csrf.js
+++ middlewares/csrf.js
@@ -3,7 +3,9 @@ const Tokens = require('csrf');
 const tokens = new Tokens();
 
 function csrfMiddleware(req, res, next) {
-  
+  if (req.path != '/cart/flash') {
+    next();
+  } else {
     if (req.method === 'GET') {
       const secret = tokens.secretSync();
       req.session.csrfSecret = secret;
@@ -25,7 +27,7 @@ function csrfMiddleware(req, res, next) {
       }
     }
     next();
-  
+  }
 }
 
 module.exports = csrfMiddleware;
